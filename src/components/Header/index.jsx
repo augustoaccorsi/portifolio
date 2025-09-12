6ca98705a19919b35b9426690e40bedb17273108
augustoaccorsi <augusto.acorsi@gmail.com>
2025-09-12 20:38:42 -0300

    Make header responsible

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,116 +1,213 @@
 import {
     HeaderContainer,
+    HeaderInner,
+    RightGroup,
     NavButtons,
     Logo,
     Link,
     SelectedLink,
     Icon,
+    HamburgerButton,
+    MobileMenu,
 } from './styles';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { Outlet, useLocation } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import { useContext } from 'react';
+import { FaGithub, FaLinkedin, FaBars, FaTimes } from 'react-icons/fa';
+import { Outlet, useLocation, NavLink } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
 import ThemeToggle from '../ThemeToggle';
 import { PortifolioContext } from '../../context/PortifolioContext';
 
 const Header = () => {
     const location = useLocation();
     const { isDarkTheme, toggleTheme } = useContext(PortifolioContext);
+    const [menuOpen, setMenuOpen] = useState(false);
 
-    const isActiveRoute = (path) => {
-        return location.pathname === path;
-    };
+    const isActiveRoute = (path) => location.pathname === path;
+
+    // Close mobile menu when route changes
+    useEffect(() => {
+        setMenuOpen(false);
+    }, [location.pathname]);
 
     return (
         <>
-            <header>
-                <HeaderContainer>
-                    <div>
-                        <NavLink to="/portifolio/">
-                            <Logo>
-                                {isDarkTheme ? (
-                                    <img
-                                        src="rex-logo-dark.svg"
-                                        alt="Portfolio Logo"
-                                        width="50"
-                                        height="50"
-                                    />
-                                ) : (
-                                    <img
-                                        src="rex-logo-light.svg"
-                                        alt="Portfolio Logo"
-                                        width="50"
-                                        height="50"
-                                    />
-                                )}
-                            </Logo>
-                        </NavLink>
-                        <div>
-                            <NavButtons>
-                                <NavLink to="/portifolio/">
-                                    {isActiveRoute('/portifolio/') ? (
-                                        <SelectedLink>Home</SelectedLink>
-                                    ) : (
-                                        <Link>Home</Link>
-                                    )}
-                                </NavLink>
-                                <NavLink to="/portifolio/about">
-                                    {isActiveRoute('/portifolio/about') ? (
-                                        <SelectedLink>About</SelectedLink>
-                                    ) : (
-                                        <Link>About</Link>
-                                    )}
-                                </NavLink>
-                                <NavLink to="/portifolio/techStack">
-                                    {isActiveRoute('/portifolio/techStack') ? (
-                                        <SelectedLink>Tech Stack</SelectedLink>
-                                    ) : (
-                                        <Link>Tech Stack</Link>
-                                    )}
-                                </NavLink>
-                                <NavLink to="/portifolio/projects">
-                                    {isActiveRoute('/portifolio/projects') ? (
-                                        <SelectedLink>Projects</SelectedLink>
-                                    ) : (
-                                        <Link>Projects</Link>
-                                    )}
-                                </NavLink>
-                                <NavLink to="/portifolio/contact">
-                                    {isActiveRoute('/portifolio/contact') ? (
-                                        <SelectedLink>Contact</SelectedLink>
-                                    ) : (
-                                        <Link>Contact</Link>
-                                    )}
-                                </NavLink>
-                                <NavLink
-                                    to="https://github.com/augustoaccorsi"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <Icon>
-                                        <FaGithub />
-                                    </Icon>
-                                </NavLink>
-                                <NavLink
-                                    to="https://www.linkedin.com/in/augusto-accorsi/"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <Icon>
-                                        {' '}
-                                        <FaLinkedin />
-                                    </Icon>
-                                </NavLink>
+            <HeaderContainer>
+                <HeaderInner>
+                    <NavLink to="/portifolio/">
+                        <Logo>
+                            <img
+                                src={
+                                    isDarkTheme
+                                        ? 'rex-logo-dark.svg'
+                                        : 'rex-logo-light.svg'
+                                }
+                                alt="Portfolio Logo"
+                                width="50"
+                                height="50"
+                            />
+                        </Logo>
+                    </NavLink>
 
-                                <ThemeToggle
-                                    isDark={isDarkTheme}
-                                    onToggle={toggleTheme}
-                                />
-                            </NavButtons>
-                        </div>
-                    </div>
-                </HeaderContainer>
-            </header>
+                    <RightGroup>
+                        {/* Desktop nav (hidden on mobile via CSS) */}
+                        <NavButtons>
+                            <NavLink to="/portifolio/">
+                                {isActiveRoute('/portifolio/') ? (
+                                    <SelectedLink>Home</SelectedLink>
+                                ) : (
+                                    <Link>Home</Link>
+                                )}
+                            </NavLink>
+
+                            <NavLink to="/portifolio/about">
+                                {isActiveRoute('/portifolio/about') ? (
+                                    <SelectedLink>About</SelectedLink>
+                                ) : (
+                                    <Link>About</Link>
+                                )}
+                            </NavLink>
+
+                            <NavLink to="/portifolio/techStack">
+                                {isActiveRoute('/portifolio/techStack') ? (
+                                    <SelectedLink>Tech Stack</SelectedLink>
+                                ) : (
+                                    <Link>Tech Stack</Link>
+                                )}
+                            </NavLink>
+
+                            <NavLink to="/portifolio/projects">
+                                {isActiveRoute('/portifolio/projects') ? (
+                                    <SelectedLink>Projects</SelectedLink>
+                                ) : (
+                                    <Link>Projects</Link>
+                                )}
+                            </NavLink>
+
+                            <NavLink to="/portifolio/contact">
+                                {isActiveRoute('/portifolio/contact') ? (
+                                    <SelectedLink>Contact</SelectedLink>
+                                ) : (
+                                    <Link>Contact</Link>
+                                )}
+                            </NavLink>
+
+                            {/* external links should be anchor tags */}
+                            <a
+                                href="https://github.com/augustoaccorsi"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <Icon>
+                                    <FaGithub />
+                                </Icon>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/augusto-accorsi/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <Icon>
+                                    <FaLinkedin />
+                                </Icon>
+                            </a>
+
+                            <ThemeToggle
+                                isDark={isDarkTheme}
+                                onToggle={toggleTheme}
+                            />
+                        </NavButtons>
+
+                        {/* Hamburger (visible on small screens) */}
+                        <HamburgerButton
+                            aria-label={menuOpen ? 'Close menu' : 'Open menu'}
+                            aria-expanded={menuOpen}
+                            aria-controls="mobile-menu"
+                            onClick={() => setMenuOpen((s) => !s)}
+                            type="button"
+                        >
+                            {menuOpen ? <FaTimes /> : <FaBars />}
+                        </HamburgerButton>
+                    </RightGroup>
+                </HeaderInner>
+
+                {/* Mobile menu (animated via CSS). We use data-open attribute to control visibility */}
+                <MobileMenu
+                    id="mobile-menu"
+                    data-open={menuOpen ? 'true' : 'false'}
+                >
+                    <NavLink
+                        to="/portifolio/"
+                        onClick={() => setMenuOpen(false)}
+                    >
+                        {isActiveRoute('/portifolio/') ? (
+                            <SelectedLink>Home</SelectedLink>
+                        ) : (
+                            <Link>Home</Link>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to="/portifolio/about"
+                        onClick={() => setMenuOpen(false)}
+                    >
+                        {isActiveRoute('/portifolio/about') ? (
+                            <SelectedLink>About</SelectedLink>
+                        ) : (
+                            <Link>About</Link>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to="/portifolio/techStack"
+                        onClick={() => setMenuOpen(false)}
+                    >
+                        {isActiveRoute('/portifolio/techStack') ? (
+                            <SelectedLink>Tech Stack</SelectedLink>
+                        ) : (
+                            <Link>Tech Stack</Link>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to="/portifolio/projects"
+                        onClick={() => setMenuOpen(false)}
+                    >
+                        {isActiveRoute('/portifolio/projects') ? (
+                            <SelectedLink>Projects</SelectedLink>
+                        ) : (
+                            <Link>Projects</Link>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to="/portifolio/contact"
+                        onClick={() => setMenuOpen(false)}
+                    >
+                        {isActiveRoute('/portifolio/contact') ? (
+                            <SelectedLink>Contact</SelectedLink>
+                        ) : (
+                            <Link>Contact</Link>
+                        )}
+                    </NavLink>
+
+                    <a
+                        href="https://github.com/augustoaccorsi"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Icon>
+                            <FaGithub />
+                        </Icon>
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/augusto-accorsi/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Icon>
+                            <FaLinkedin />
+                        </Icon>
+                    </a>
+
+                    <ThemeToggle isDark={isDarkTheme} onToggle={toggleTheme} />
+                </MobileMenu>
+            </HeaderContainer>
+
             <Outlet />
         </>
     );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,86 +1,142 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.div`
+export const HeaderContainer = styled.header`
+    position: relative;
     margin-top: 2rem;
     margin-bottom: 3rem;
-    background: ${(props) => props.theme['base-card']};
+    background: ${(p) => p.theme['base-card']};
     border-radius: 12px;
-    padding: 1.5rem 2rem;
+    padding: 1rem 1rem;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
     border-bottom: 3px solid transparent;
+    
     background-image: linear-gradient(${(props) => props.theme['base-card']}, ${(props) => props.theme['base-card']}),
                       linear-gradient(135deg, ${(props) => props.theme['purple']}, ${(props) => props.theme['yellow']});
-    
+                      
     background-origin: border-box;
     background-clip: padding-box, border-box;
     transition: all 0.3s ease;
+`;
 
-    div {
-        display: flex;
+export const HeaderInner = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+`;
+
+/* group to keep logo and controls aligned */
+export const RightGroup = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+`;
+
+/* MOBILE-FIRST: hide full nav by default and show hamburger. On wider screens show nav and hide hamburger */
+export const NavButtons = styled.nav`
+    display: none; /* hidden on small screens */
+
+    a,
+    span {
+        padding: 0.5rem 0.75rem;
+        text-decoration: none;
+        display: inline-flex;
         align-items: center;
-        justify-content: space-between;
+        justify-content: center;
+    }
+
+    @media (min-width: 769px) {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
     }
 `;
 
-export const NavButtons = styled.div`
-    gap: 1rem;
+export const HamburgerButton = styled.button`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background: none;
+    border: 1px solid transparent;
+    padding: 0.35rem;
+    font-size: 1.4rem;
+    line-height: 1;
+    border-radius: 8px;
+    cursor: pointer;
+    color: ${(p) => p.theme['base-text']};
+
+    &:hover {
+        background: rgba(0, 0, 0, 0.04);
+    }
+
+    @media (min-width: 769px) {
+        display: none; /* hide on desktop */
+    }
+`;
+
+export const MobileMenu = styled.div`
+    /* hidden by default (data-open controls visibility) */
+    display: none;
+    position: absolute;
+    top: calc(100% + 0.6rem);
+    right: 1rem;
+    left: 1rem;
+    z-index: 100;
+    padding: 0.75rem;
+    border-radius: 10px;
+    box-shadow: 0 6px 18px rgba(0, 0, 0, 0.08);
+    background: ${(p) => p.theme['base-card']};
+    transform-origin: top center;
+    transition: transform 180ms ease, opacity 180ms ease;
+    opacity: 0;
+    transform: translateY(-6px) scaleY(0.98);
+    pointer-events: none;
+
+    /* show only on small screens; we'll toggle visibility with the data attribute */
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+
+    /* when open via data-open="true" */
+    &[data-open='true'] {
+        opacity: 1;
+        transform: translateY(0) scaleY(1);
+        pointer-events: auto;
+    }
 
     a {
-        background: none;
-        padding: 0.5rem 1rem;
-        border: none;
-        border-radius: 8px;
         text-decoration: none;
-        transition: all 0.3s ease;
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
-
-        &:disabled:hover {
-            cursor: not-allowed;
-        }
+        padding: 0.5rem;
     }
 `;
 
 export const Logo = styled.div`
-    position: relative;
-    
     img {
-        transition: all 0.3s ease;
+        transition: transform 150ms ease;
         border-radius: 8px;
         padding: 0.25rem;
     }
-    
-    &:hover {
-        cursor: pointer;
-        
-        img {
-            transform: scale(1.1);
-            /* background: ${(props) => props.theme['purple']}; */
-            border-radius: 12px;
-        }
+    &:hover img {
+        transform: scale(1.06);
     }
 `;
 
 export const Link = styled.span`
-    color: ${(props) => props.theme['base-text']};
+    color: ${(p) => p.theme['base-text']};
     font-weight: 500;
-    transition: all 0.3s ease;
-    
+    transition: color 150ms ease;
     &:hover {
+        color: ${(p) => p.theme['purple-dark']};
         cursor: pointer;
-        color: ${(props) => props.theme['purple-dark']};
     }
 `;
 
 export const SelectedLink = styled.span`
-    color: ${(props) => props.theme['base-title']};
+    color: ${(p) => p.theme['base-title']};
     font-weight: 600;
     position: relative;
-    transition: all 0.3s ease;
-
     &::after {
         content: '';
         position: absolute;
@@ -88,26 +144,26 @@ export const SelectedLink = styled.span`
         left: 0;
         right: 0;
         height: 2px;
-        background: ${(props) => props.theme['purple']};
+        background: ${(p) => p.theme['purple']};
         border-radius: 1px;
     }
-
     &:hover {
         cursor: pointer;
-        color: ${(props) => props.theme['purple']};
-        transform: scale(1.05);
+        color: ${(p) => p.theme['purple']};
     }
 `;
 
 export const Icon = styled.div`
-    color: ${(props) => props.theme['base-text']};
-    font-size: 1.2rem;
-    padding: 0.5rem;
+    color: ${(p) => p.theme['base-text']};
+    font-size: 1.1rem;
+    padding: 0.4rem;
     border-radius: 8px;
-    transition: all 0.3s ease;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
 
     &:hover {
+        color: ${(p) => p.theme['purple']};
         cursor: pointer;
-        color: ${(props) => props.theme['purple']};
     }
 `;
